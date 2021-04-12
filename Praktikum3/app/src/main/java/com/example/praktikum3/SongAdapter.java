276300b8bcf5da0d7dd680643a50fcef04ca7e50
salsabillaflsft <50274165+salsabillaflsft@users.noreply.github.com>
2021-04-13 01:58:53 +0700

    fix

--- Praktikum3/app/src/main/java/com/example/praktikum3/SongAdapter.java
+++ Praktikum3/app/src/main/java/com/example/praktikum3/SongAdapter.java
@@ -2,22 +2,16 @@ package com.example.praktikum3;
 
 import android.content.Context;
 import android.content.Intent;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
 
